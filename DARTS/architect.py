fbb2ebbf2a9051f1d7072af5894f773ac538eeae
Antoine Yang <49347253+antoyang@users.noreply.github.com>
2020-06-18 21:38:37 +0200

    Update architect.py

--- DARTS/architect.py
+++ DARTS/architect.py
@@ -105,5 +105,5 @@ class Architect():
             for p, d in zip(self.net.weights(), dw):
                 p += eps * d
 
-        hessian = [(p-n) / 2.*eps for p, n in zip(dalpha_pos, dalpha_neg)]
+        hessian = [(p-n) / (2.*eps) for p, n in zip(dalpha_pos, dalpha_neg)]
         return hessian
